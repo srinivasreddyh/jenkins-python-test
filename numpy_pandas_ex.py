3246e658a2c1079ad483c4c91552fb18f76f0902
srinivasreddyh <shrini071@gmail.com>
2019-02-15 15:05:57 +0530

    adding files

---
+++ numpy_pandas_ex.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+p_data=[{1:'dell',2:'lenovo',3:'hp',4:'vaio'}]
+df = pd.DataFrame(p_data)
+
+n_data=['xperia','moto','nokia','oneplus']
+n = np.array(n_data)
+
+print "pandas output is...."
+print df
+
+print "numpy output is...."
+print n
