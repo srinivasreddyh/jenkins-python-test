75acbe1aa87c0108e4dcca7611029478c5cc89a3
srinivasreddyh <shrini071@gmail.com>
2019-02-18 11:57:12 +0530

    creating new file

---
+++ pythonfiles/generators_fun_ex.py
@@ -0,0 +1,11 @@
+#simple generator function
+
+def gen_ints(m):
+    for i in range(m):
+        yield i
+gen=gen_ints(4)
+print gen
+print gen.next()
+print gen.next()
+print gen.next()
+print gen.next()
