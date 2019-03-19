2cbceb0af44aa7a5d03e960d9be9de0fd93aa953
srinivasreddyh <shrini071@gmail.com>
2019-03-19 08:57:44 +0530

    changes in rename

--- rename.py
+++ rename.py
@@ -1,8 +1,9 @@
 import os
 
-job_name = os.environ.get('JOB_NAME', 'default_name')
-build_number = os.environ.get('BUILD_NUMBER', 1)
-new_output = "{}_{}.bin".format(job_name, build_number)
+#job_name = os.environ.get('JOB_NAME', 'default_name')
+#build_number = os.environ.get('BUILD_NUMBER', 1)
+#new_output = "{}_{}.bin".format(job_name, build_number)
 
 original_output="/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_os.pkl"
+new_output="/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_osnew.pkl"
 os.rename(original_output, new_output)	
