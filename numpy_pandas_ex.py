229bde4765a4172cee771ee5fa24f46296537635
srinivasreddyh <shrini071@gmail.com>
2019-03-19 07:58:48 +0530

    changes made to numpy3

--- numpy_pandas_ex.py
+++ numpy_pandas_ex.py
@@ -21,7 +21,7 @@ print n
 
 with open("/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_os.pkl","wb") as f:
 	pickle.dump(df,f)
-
+'''
 import os
 job_name = os.environ.get('JOB_NAME', 'default_name')
 build_number = os.environ.get('BUILD_NUMBER', 1)
@@ -29,6 +29,7 @@ new_output = "{}_{}.bin".format(job_name, build_number)
 
 original_output="/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_os.pkl"
 os.rename(original_output, new_output)	
+'''
 '''
 import jenkins
 next_bn = server.get_job_info('job_name')['nextBuildNumber']
