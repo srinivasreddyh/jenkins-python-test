76f843ac3052362b15c31858705759a5809595bb
srinivasreddyh <shrini071@gmail.com>
2019-03-19 09:07:10 +0530

    remove rename

--- rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#job_name = os.environ.get('JOB_NAME', 'default_name')
-#build_number = os.environ.get('BUILD_NUMBER', 1)
-#new_output = "{}_{}.bin".format(job_name, build_number)
-
-original_output="/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_os.pkl"
-new_output="/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/gen_output_osnew.pkl"
-os.rename(original_output, new_output)	
