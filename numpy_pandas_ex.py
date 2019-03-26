e9d9078e7564bb170e03d90f45821c892574e064
srinivasreddyh <shrini071@gmail.com>
2019-03-26 17:49:05 +0530

    changes in numpy

--- numpy_pandas_ex.py
+++ numpy_pandas_ex.py
@@ -23,7 +23,7 @@ print "numpy output is...."
 print n
 
 J = Jenkins('http://localhost:8080', username='srinivasreddyh', password='meghana3')
-build_no=str(J['master'].get_last_good_build())[-3:]
+build_no=str(J['master'].get_last_build())[-3:]
 print "Last Successful BUILD_NUMBER :",build_no
 
 filename = "/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/%s.pkl" %build_no
