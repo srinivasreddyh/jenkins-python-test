3626229ffd991e819f0c63ab578ed144e770c867
srinivasreddyh <shrini071@gmail.com>
2019-03-27 16:14:08 +0530

    changes to read reports

--- read_reports.py
+++ read_reports.py
@@ -6,8 +6,12 @@ J = Jenkins('http://localhost:8080', username='srinivasreddyh', password='meghan
 build_no=str(J['master'].get_last_build())[-3:]
 print "Last BUILD_NUMBER :",build_no
 
-# to read pickle file
+# to read pickle file of current build
 filename = "/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/%s.pkl" %build_no
 with open(filename ,"rb") as f2:
     new_data=pickle.load(f2)
 print new_data
+
+last_build_no=str(J['master'].get_last_good_build())[-3:]
+print "Previous BUILD_NUMBER :",last_build_no
+print "Last BUILD_NUMBER :",build_no
