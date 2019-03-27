b04dfa37af1b0bfd37f3246475d131278c678ece
srinivasreddyh <shrini071@gmail.com>
2019-03-27 15:39:15 +0530

    changes in numpy

--- numpy_pandas_ex.py
+++ numpy_pandas_ex.py
@@ -5,10 +5,7 @@ import generators_fun_ex
 
 import pandas as pd
 import numpy as np
-import pickle
 
-# using jenkinsapi
-from jenkinsapi.jenkins import Jenkins
 
 p_data=[{1:'dell',2:'lenovo',3:'hp',4:'vaio'}]
 df = pd.DataFrame(p_data)
@@ -21,13 +18,5 @@ print df
 
 print "numpy output is...."
 print n
-
-J = Jenkins('http://localhost:8080', username='srinivasreddyh', password='meghana3')
-build_no=str(J['master'].get_last_build())[-3:]
-print "Last BUILD_NUMBER :",build_no
-
-filename = "/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/%s.pkl" %build_no
-with open(filename ,"wb") as f:
-    pickle.dump(df,f)
 
 
