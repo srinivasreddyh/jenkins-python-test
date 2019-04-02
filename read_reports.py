a87ce33ea62c55fb568ffa438e84f2c6d4ccd7a6
srinivas <shrini071@gmail.com>
2019-04-02 16:20:55 +0530

    changes in comment lines2

--- read_reports.py
+++ read_reports.py
@@ -14,9 +14,7 @@ with open(filename ,"rb") as f1:
     new_data1=pickle.load(f1)
 print "\ncurrent BUILD data :",new_data1
 
-
 # to get previous BUILD_NUMBER
-'''
 last_build_no=str(J['master'].get_last_good_build())[-3:]
 print "Previous BUILD_NUMBER :",last_build_no
 
@@ -25,4 +23,3 @@ filename = "/var/lib/jenkins/workspace/jenkins-python-test_master@tmp/%s.pkl" %l
 with open(filename ,"rb") as f2:
     new_data2=pickle.load(f2)
 print "\nprevious BUILD data :",new_data2
-'''
